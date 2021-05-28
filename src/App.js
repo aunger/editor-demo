2424a48f686bf7b0a8d03a5dfa30fc9c4bc93a1d
Russell Aunger <rba@live.com>
2021-05-28 01:31:02 -0400

    Use some HOCs from USFM-editor, and load source from door43.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { createBasicUsfmEditor, withChapterPaging, withChapterSelection, withToolbar } from "usfm-editor"
 import './App.css';
-import { BasicUsfmEditor } from "usfm-editor"
+import { DemoToolbarSpecs } from './DemoToolbarSpecs'
 
-const usfm = String.raw`
-\id MRK Unlocked Literal Bible
+const loading = String.raw`
+\id XXA LOADING
 \ide UTF-8
-\h Mark
-\toc1 The Gospel of Mark
-\toc2 Mark
-\toc3 Mrk
-\mt Mark
-
-\s5
 \c 1
 \p
-\v 1 This is the beginning of the gospel of Jesus Christ, the Son of God.
-\p
-\v 2 As it is written in Isaiah the prophet,
-\q "Look, I am sending my messenger before your face,
-\q the one who will prepare your way.
-\q
-\v 3 The voice of one calling out in the wilderness,
-\q 'Make ready the way of the Lord;
-\q make his paths straight.'"
-\m
-
-\s5
-\p
-\v 4 John came, baptizing in the wilderness and preaching a baptism of repentance for the forgiveness of sins.
-\v 5 The whole country of Judea and all the people of Jerusalem went out to him. They were baptized by him in the Jordan River, confessing their sins.
-\v 6 John wore a coat of camel's hair and a leather belt around his waist, and he ate locusts and wild honey.
+\v 1 Loading, please wait...
 `.trimLeft()
 
 function App() {
+  const CustomEditor = withToolbar(withChapterSelection(withChapterPaging(createBasicUsfmEditor())))
+
+  const [sourceString, setSourceString] = useState(loading);
+  useEffect(() => {
+    window.fetch("https://git.door43.org/unfoldingWord/en_ult/raw/tag/25/27-DAN.usfm")
+          .then(r=> r.text())
+          .then(text => { setSourceString(text) })
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -40,7 +28,10 @@ function App() {
           USFM Editor
         </p>
       </header>
-      <BasicUsfmEditor usfmString={usfm} />
+      <CustomEditor
+        usfmString={sourceString}
+        toolbarSpecs={DemoToolbarSpecs}
+        />
     </div>
   );
 }
